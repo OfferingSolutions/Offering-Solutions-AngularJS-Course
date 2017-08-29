557e4bd7438b41e8dfc4be562332d6f44bcb3f58
FabianGosebrink <fabian.gosebrink@outlook.com>
2017-08-29 07:55:33 +0200

    updated all package.jsons

---
+++ AngularJS-Course/updatePackageJson.ps1
@@ -0,0 +1,27 @@
+﻿Get-ChildItem -recurse | 
+    where {$_.name -eq "package.json"} | 
+    foreach { 
+        cd $_.DirectoryName; 
+        Write-Output $_.DirectoryName;       
+        
+        if ( Test-Path "./node_modules" ) {
+            echo $_.DirectoryNamecls;
+            rimraf node_modules
+        }
+
+        
+        if ( Test-Path "./package-lock.json" ) {
+            echo $_.DirectoryNamecls;
+             Remove-Item .\package-lock.json -Force;
+        }
+		
+		if ( Test-Path "./yarn.lock" ) {
+            echo $_.DirectoryNamecls;
+             Remove-Item .\yarn.lock -Force;
+        }
+
+        ncu -a
+
+        <# Remove-Item .\node_modules -Force -Recurse #>
+    }
+    cd ..
